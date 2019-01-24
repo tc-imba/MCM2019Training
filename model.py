51ff70e922afd7ce79c640907db1d86be81797bc
tc-imba <liuyh615@sjtu.edu.cn>
2019-01-24 15:02:50 +0800

    model

---
+++ model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+df = pd.read_csv('data.csv')
+df['订单创建时间'] = df['订单创建时间'].map(lambda x: x[0:10])
+
+df_orange = df[df['商品名称'] == '广西沙糖桔']
+
+g_items = df_orange.groupby('订单创建时间').sum()
+df = pd.DataFrame(g_items)
+print(df)
+
+
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+df.plot(y='商品数量')
+plt.show()
